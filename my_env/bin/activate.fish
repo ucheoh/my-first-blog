0ea12d839de83f908a0a032ca6a43be87bcb8549
UO <schoolgirluche@protonmail.com>
2023-01-19 08:14:59 -0800

    upd pro name

--- my_env/bin/activate.fish
+++ my_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/uo/Documents/django_projects/django_project1/myenv"
+set -gx VIRTUAL_ENV "/Users/uo/Documents/django_projects/django_project_1/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
